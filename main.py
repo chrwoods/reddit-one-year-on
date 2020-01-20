2ec59f24de42f0d73ba641701bde4d37ff13615a
Christopher Woods <chrisscottwoods@gmail.com>
2020-01-19 18:15:42 -0600

    basic praw setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import praw
+import config
+
+reddit = praw.Reddit(client_id=config.CLIENT_ID,
+                     client_secret=config.CLIENT_SECRET,
+                     user_agent=config.USER_AGENT,
+                     username=config.USERNAME,
+                     password=config.PASSWORD)
+
+print(reddit.read_only)
