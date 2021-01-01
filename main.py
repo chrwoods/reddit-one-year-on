34511d7c620001e59ce65823ee1a2af24ac3c582
Christopher Woods <chrisscottwoods@gmail.com>
2020-12-31 21:27:40 -0600

    Added comment creation, fixed enum

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import praw
 import config
 
-checkin_msg = '''%s
+message = '''/u/%s
 
 Two years ago you posted what you wanted to achieve in 2019, and today I'm messaging you to see how you did over the past 24 months!
 
@@ -16,17 +16,27 @@ reddit = praw.Reddit(client_id=config.CLIENT_ID,
                      password=config.PASSWORD)
 
 post = reddit.submission(id="a9yzx0")
+# post.comment_sort = 'new'
+
 print(post.title)
 
-post.comments.replace_more(limit=0)
-comments = list(post.comments)
+post.comments.replace_more(limit=None)
+comments = post.comments
 
+print(len(comments))
 count = 0
 for comment in comments:
     if comment.author is None:
         continue
+    if comment.author.name != 'ChrisMan174':
+        continue
+    
     print(comment.author.name)
     print(comment.body)
+    indented_body = comment.body.replace('\n', '\n> ')
+
+    print(message % (comment.author.name, indented_body))
+    
     count += 1
     if count > 10:
         break
